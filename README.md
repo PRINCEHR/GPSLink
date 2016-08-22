d97f1d45a6015a12ebce801024476fc7d718763c
Michael Schwartz <mikethedj4@yahoo.com>
2016-08-22 05:41:03 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+GPSLink
+===================
+
+Easily share your GPS location with your friends or family!
+
+Version
+-------------
+
+0.0.1
+
+License
+-------------
+
+MIT
+
+Tech
+-------------
+
+GPSLink uses a number of open source projects to work properly:
+
+* [jQuery](http://jquery.com/) - duh
+* [Google Maps](http://maps.google.com/) - duh
+* [Modernizer](https://modernizr.com/) - A JavaScript library that detects HTML5 and CSS3 features in the user's browser.
+
+Development
+-------------
+
+GPSLink was created by [Michael Schwartz](http://mikethedj4.github.io/).  
+
+Want to contribute? Great!  
+
+If you have feedback or feature requests, [please add/vote here](https://github.com/mikethedj4/GPSLink/issues).  
+
+Of course GPSLink is free and open source, so you can always fork the project and have fun :)
+
+If GPSLink was at all helpful for you. [Would you consider donating to the project?](https://www.paypal.com/us/cgi-bin/webscr?cmd=_flow&SESSION=JryIEtO_GiYnqlvRfV6BGnO6bAxR3JtIQif2j1z1eFYuoLkYf_XZOY6QbWe&dispatch=5885d80a13c0db1f8e263663d3faee8dcce3e160f5b9538489e17951d2c62172)
---
+++ gpslink.js
@@ -0,0 +1,94 @@
+var sb,sa,sb1,sb2,
+    hash     = window.location.hash,
+    locVal   = document.querySelector("[data-grab=locval]"),
+    locBtn   = document.querySelector("[data-grab=locbtn]"),
+    mapFrame = document.querySelector("[data-call=map]");
+
+/*
+API Key
+AIzaSyDDotKqMh0idkF4ZK0-zgBy0gXZFI9nm0o
+*/
+
+var shareLocation = function() {
+  if ('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      sa = position.coords.latitude;
+      sb = position.coords.longitude;
+      locVal.value = sa + "," + sb;
+      window.location.hash = locVal.value;
+
+      document.querySelector(".frame").innerHTML = "";
+      var frame = document.createElement("iframe");
+      frame.setAttribute("id", "frame");
+      frame.setAttribute("sandbox", "allow-forms allow-modals allow-pointer-lock allow-popups allow-same-origin allow-scripts");
+      document.querySelector(".frame").appendChild(frame);
+      var mapFrame = document.getElementById("frame");
+      mapFrame.src = "frame.html#" + window.location.hash;
+      locBtn.value = "Share";
+    });
+  }
+}
+
+if (hash) {
+  // Grab URL Hash
+  locVal.value = sb = sa = hash.toString().replace(/#/g,'');
+  mapFrame.src = "frame.html#" + locVal.value;
+
+  // Remove all text before character
+  sb = sb.split(",").pop();
+  // Remove all text after character
+  sa = sa.substring(0, sa.indexOf(','));
+  // Set location in textbox
+  var LatnLng = sa + "," + sb;
+  
+  locBtn.value = "Locate Me";
+} else {
+  shareLocation();
+}
+
+// Change Map Location
+locVal.onkeyup = function(e) {
+  if (!locVal.value) {
+    locBtn.value = "Locate Me";
+    if (e.keyCode == 13) {
+      shareLocation();
+    }
+  } else {
+    locBtn.value = "Go To";
+    if (e.keyCode == 13) {
+      locBtn.click();
+    }
+  }
+  
+  if (window.location.hash === this.value) {
+    locBtn.value = "Share";
+    if (e.keyCode == 13) {
+      locBtn.click();
+    }
+  }
+}
+
+// Click Button To Share Your Location
+locBtn.onclick = function() {
+  if (!locVal.value) {
+    if (e.keyCode == 13) {
+      shareLocation();
+    }
+  } else if (locBtn.value === "Locate Me") {
+    locBtn.value = "Loading";
+    locVal.value = window.location.hash.toString().replace(/#/g,'');
+    shareLocation();
+  } else if (locBtn.value === "Share") {
+    locVal.value = window.location.href;
+  } else if (locBtn.value === "Go To") {
+    window.location.hash = locVal.value.replace(/#/g,'');
+    document.querySelector(".frame").innerHTML = "";
+    var frame = document.createElement("iframe");
+    frame.setAttribute("id", "frame");
+    frame.setAttribute("sandbox", "allow-forms allow-modals allow-pointer-lock allow-popups allow-same-origin allow-scripts");
+    document.querySelector(".frame").appendChild(frame);
+    var mapFrame = document.getElementById("frame");
+    mapFrame.src = "frame.html#" + locVal.value.replace(/#/g,'');
+    locBtn.value = "Share";
+  }
+};
